09e977651794da685b2935bcf149e09624c00f0a
LornaU <70303875+LornaIU@users.noreply.github.com>
2021-10-02 14:42:04 +0800

    Create license.py

---
+++ license.py
@@ -0,0 +1,22 @@
+import license_module as m 
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+if capture.isOpened():
+    while True:
+        sucess, img = capture.read()
+        if sucess:
+            cv2.imshow("Frame", img)
+        k = cv2.waitKey(100)
+        if k == ord('s') or k ==ord("S"):
+            cv2.imwrite('short.jpg',img)
+            text = m.get_license(img)
+            print("車牌:",text)
+        if k == ord("q")or k ==ord("Q"):
+            print("exit")
+            cv2.destroyAllWindows()
+            capture.release()
+            break
+    else:
+        print("開啟攝影機失效")
