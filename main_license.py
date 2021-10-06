e21e8c7d76776e771eefd159e20afafbbfdfbc28
LornaU <70303875+LornaIU@users.noreply.github.com>
2021-10-06 20:21:08 +0800

    Update main_license.py

--- main_license.py
+++ main_license.py
@@ -1,4 +1,4 @@
-import get_carlicense as g 
+import get_carlicense as m 
 import cv2
 
 capture = cv2.VideoCapture(0)
@@ -11,7 +11,7 @@ if capture.isOpened():
         k = cv2.waitKey(100)
         if k == ord('s') or k ==ord("S"):
             cv2.imwrite('short.jpg',img)
-            text = g.get_license(img)
+            text = m.get_license(img)
             print("車牌:",text)
         if k == ord("q")or k ==ord("Q"):
             print("exit")
